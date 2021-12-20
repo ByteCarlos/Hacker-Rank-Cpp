a05ced1030e9ed839ba8d055476389f282838f85
Carlos Henrique Santos Silveira <91327797+EngCarlosSilveira@users.noreply.github.com>
2021-12-20 13:30:09 -0300

    Update InputAndOutput.cpp

--- C++/InputAndOutput.cpp
+++ C++/InputAndOutput.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int main() {
-    int numeros[3];
-    for(int i = 0; i < 3; i ++){
-        int valor;
-        cout << "Qual o numero na posicao " << i << "?" << endl;
-        cin >> valor;
-        numeros[i] = valor;
-    }
-    int soma = 0;
-    for(int i = 0; i < 3; i++)
-        soma += numeros[i];
+    int num1;
+    int num2;
+    int num3;
+
+    cin >> num1 >> num2 >> num3;
+    int soma = num1 + num2 + num3;
+
     cout << soma << endl;
     return 0;
 }
